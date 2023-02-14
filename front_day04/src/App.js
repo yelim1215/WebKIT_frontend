ccf54f5237d829e071cedaac8da9493062fce0cd
yelim1215 <yerim1215jg@naver.com>
2023-02-14 09:45:06 +0900

    230214_0945

--- front_day04/src/App.js
+++ front_day04/src/App.js
@@ -1,18 +1,46 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function ItemRow({ item, removeItem }) {
+function ItemRow({ item, removeItem, updateItem }) {
+  const [mode, setMode] = useState(false);
+  const [title, setTitle] = useState(item.title);
   return (
     <li>
       <p>
-        <input type="checkbox" />
-        <input type="text" value={item.title} disabled />
+        <input
+          type="checkbox"
+          onChange={(e) => {
+            item.done = e.target.checked;
+            updateItem(item);
+          }}
+        />
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          className={item.done ? "done" : "not-done"}
+          type="text"
+          disabled={mode ? "" : "disabled"}
+        />
         <button
           onClick={(e) => {
             removeItem(item.no);
           }}
         >
           삭제
+        </button>
+        <button
+          onClick={(e) => {
+            setMode(!mode);
+            if (mode) {
+              item.title = title;
+              updateItem(item);
+            } else {
+            }
+          }}
+        >
+          {mode ? "수정완료" : "수정"}
         </button>
       </p>
     </li>
@@ -44,19 +72,28 @@ function InputItem({ appendItem }) {
 }
 
 // Redux를 이용하면 해결된다.
-function TodoList({ todoList, removeItem }) {
+function TodoList({ todoList, removeItem, updateItem }) {
   return (
     <div>
-      <ul>
+      <ol>
         {todoList.map((item, idx) => {
-          return <ItemRow key={item.no} item={item} removeItem={removeItem} />;
+          return (
+            <ItemRow
+              key={item.no}
+              item={item}
+              removeItem={removeItem}
+              updateItem={updateItem}
+            />
+          );
         })}
-      </ul>
+      </ol>
     </div>
   );
 }
 
 function App(props) {
+  // 과제 1 : 취소선 기능 추가.
+  // 과제 2 : todoList 데이터를 localStorage에 저장.
   const [todoList, setTodoList] = useState([
     { no: 1, title: "점심 먹기", done: false },
     { no: 2, title: "산책 하기", done: false },
@@ -76,12 +113,27 @@ function App(props) {
     });
     setTodoList(newList);
   }
+
+  function updateItem(item) {
+    //console.dir("updateItem: " + JSON.stringify(item)) ;
+    const idx = todoList.findIndex((todo, idx) => {
+      return todo.no === item.no;
+    });
+    todoList[idx] = item;
+    setTodoList([...todoList]);
+    console.dir(todoList);
+  }
+
   return (
     <>
       <h1>Todo List</h1>
       <InputItem appendItem={appendItem} />
       <hr />
-      <TodoList todoList={todoList} removeItem={removeItem} />
+      <TodoList
+        todoList={todoList}
+        removeItem={removeItem}
+        updateItem={updateItem}
+      />
     </>
   );
 }
